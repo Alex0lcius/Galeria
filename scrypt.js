e0dab26708ccb96c6eb5258a130b022286e6f983
Alex <161379375+Alex0lcius@users.noreply.github.com>
2024-03-25 23:20:24 +0100

    Add files via upload

---
+++ scrypt.js
@@ -0,0 +1,23 @@
+function r1() {
+	document.getElementById("zadanie").innerHTML = '<img src="r1.svg">';
+}
+
+function r2() {
+	document.getElementById("zadanie").innerHTML = '<img src="r2.svg">';
+}
+
+function r3() {
+	document.getElementById("zadanie").innerHTML = '<img src="r3.svg">';
+}
+
+function r4() {
+	document.getElementById("zadanie").innerHTML = '<img src="r4.png">';
+}
+
+function r5() {
+	document.getElementById("zadanie").innerHTML = '<img src="r5.svg">';
+}
+
+function r6() {
+	document.getElementById("zadanie").innerHTML = '<img src="r6.svg">';
+}
